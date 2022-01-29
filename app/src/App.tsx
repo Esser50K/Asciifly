cd939265a0d8580cc48b28fe4acae8bbc3404983
André Esser <bepandre@hotmail.com>
2022-01-29 23:29:48 +0000

    polishing a lot of the features and handling errors

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,31 @@
 import React, { FormEvent, useEffect, useRef, useState } from 'react';
 import './App.css';
 
+enum PlayerState {
+  Empty,
+  Loading,
+  Playing
+}
+
 function App() {
   const imgInput = useRef(null)
+  const audioPlayer = useRef(null)
   const [ytUrl, setYtUrl] = useState("")
   const [audioUrl, setAudioUrl] = useState("")
   const [playerContent, setPlayerContent] = useState("")
   const [fileIsHovering, setFileIsHovering] = useState(false)
   const [wrongFileType, setWrongFileType] = useState(false)
+  const [loadingIdx, setLoadingIdx] = useState(0)
+  const [playerState, setPlayerState] = useState(PlayerState.Empty)
   const [lineLength, setLineLength] = useState(0)
   const [nLines, setNLines] = useState(0)
 
   const supportedImageTypes = ["image/jpeg", "image/jpg", "image/png"]
+  const loading = [
+    `    __                      __ _                  \n   / /   ____   ____ _ ____/ /(_)____   ____ _    \n  / /   / __ \\ / __ \`// __  // // __ \\ / __ \`/    \n / /___/ /_/ // /_/ // /_/ // // / / // /_/ /  _  \n/_____/\\____/ \\__,_/ \\__,_//_//_/ /_/ \\__, /  (_) \n                                     /____/       `,
+    `    __                      __ _                       \n   / /   ____   ____ _ ____/ /(_)____   ____ _         \n  / /   / __ \\ / __ \`// __  // // __ \\ / __ \`/         \n / /___/ /_/ // /_/ // /_/ // // / / // /_/ /  _    _  \n/_____/\\____/ \\__,_/ \\__,_//_//_/ /_/ \\__, /  (_)  (_) \n                                     /____/            `,
+    `    __                      __ _                            \n   / /   ____   ____ _ ____/ /(_)____   ____ _              \n  / /   / __ \\ / __ \`// __  // // __ \\ / __ \`/              \n / /___/ /_/ // /_/ // /_/ // // / / // /_/ /  _    _    _  \n/_____/\\____/ \\__,_/ \\__,_//_//_/ /_/ \\__, /  (_)  (_)  (_) \n                                     /____/                 \n`
+  ]
 
   useEffect(() => {
     const div = imgInput.current! as HTMLDivElement
@@ -61,15 +75,14 @@ function App() {
     e.stopPropagation()
 
     setFileIsHovering(false)
-    if (wrongFileType) {
-      setWrongFileType(false)
-      return
-    }
-
     setWrongFileType(false)
     if (e.dataTransfer?.items && e.dataTransfer.items.length > 0) {
-      const scrollingElement = (document.scrollingElement || document.body);
-      scrollingElement.scrollTop = scrollingElement.scrollHeight;
+      if (!supportedImageTypes.includes(e.dataTransfer.items[0].type)) {
+        setPlayerState(PlayerState.Empty)
+        return
+      }
+
+      setPlayerState(PlayerState.Loading)
 
       const arrBuffer = await e.dataTransfer?.items[0].getAsFile()?.arrayBuffer()
       if (arrBuffer === undefined)
@@ -81,14 +94,21 @@ function App() {
       const resp = await fetch("http://127.0.0.1:8080/img", { method: "POST", body: base64String })
       if (resp.status !== 200) {
         console.error("error uploading image:", await resp.text())
+        alert("error processing image")
       }
 
       const decoded = await resp.json()
-
+      setPlayerState(PlayerState.Playing)
       setPlayerContent(decoded.img)
       setLineLength(decoded.width)
       setNLines(decoded.height)
+      scrollDown()
     }
+  }
+
+  const scrollDown = () => {
+    const scrollingElement = (document.scrollingElement || document.body);
+    scrollingElement.scrollTop = scrollingElement.scrollHeight;
   }
 
   const handleSelectImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -97,119 +117,167 @@ function App() {
       return;
     }
 
+    setPlayerState(PlayerState.Loading);
     const base64String = window.btoa(new Uint8Array(await f.arrayBuffer()).reduce(function (data, byte) {
       return data + String.fromCharCode(byte);
     }, ''));
     const resp = await fetch("http://127.0.0.1:8080/img", { method: "POST", body: base64String })
     if (resp.status !== 200) {
       console.error("error uploading image:", await resp.text())
+      alert("error processing image")
     }
 
     const decoded = await resp.json()
+    setPlayerContent("")
+    setPlayerState(PlayerState.Playing)
 
     setPlayerContent(decoded.img)
     setLineLength(decoded.width)
     setNLines(decoded.height)
+    scrollDown()
   }
 
   const onYTUrlChange = (url: string) => {
-    console.log(url);
     setYtUrl(url)
   }
 
   const onYTUrlSubmit = (e: FormEvent) => {
-    const scrollingElement = (document.scrollingElement || document.body);
-    scrollingElement.scrollTop = scrollingElement.scrollHeight;
     e.preventDefault()
     e.stopPropagation()
+    const scrollingElement = (document.scrollingElement || document.body);
+    scrollingElement.scrollTop = scrollingElement.scrollHeight;
+    setPlayerState(PlayerState.Loading)
 
     const ws = new WebSocket("ws://127.0.0.1:8080/vid")
     var firstMsg = false
+    var scrolledDown = false;
     ws.onopen = () => {
       ws.send(JSON.stringify({ url: ytUrl }))
+    }
+    ws.onerror = () => {
+      alert("error processing image")
+      setPlayerState(PlayerState.Empty)
+    }
+    ws.onclose = () => {
+      if (!firstMsg) {
+        alert("error processing image")
+      }
+      setPlayerState(PlayerState.Empty)
     }
     ws.onmessage = (msg: MessageEvent) => {
       const decoded = JSON.parse(msg.data);
       if (!firstMsg) {
         firstMsg = true
         setAudioUrl(decoded.frame)
+        setPlayerContent("")
+        setPlayerState(PlayerState.Playing)
+        const player = audioPlayer.current! as HTMLVideoElement
+        player.play()
         return
       }
+
 
       setLineLength(decoded.width)
       setNLines(decoded.height)
       setPlayerContent(decoded.frame)
+      if (!scrolledDown) {
+        scrollDown()
+        scrolledDown = true
+      }
     }
+  }
+
+  useEffect(() => {
+    if (playerState === PlayerState.Loading) {
+      setLoadingAnim()
+      return
+    }
+  }, [playerState, loadingIdx])
+
+  const setLoadingAnim = async () => {
+    setPlayerContent(loading[loadingIdx])
+    await new Promise((resolve => setTimeout(resolve, 200)))
+    setLoadingIdx((loadingIdx + 1) % loading.length)
   }
 
   // this is the character ratio of a base monospace font
   const characterRatio = 5 / 3
-  const windowPortion = 1
+  const windowPortion = 0.9
   const ratio = lineLength / nLines
   const lineHeight = ((window.innerHeight / nLines)) * windowPortion
   const fontSize = (((window.innerHeight * ratio) / lineLength) * characterRatio) * windowPortion
 
   // eslint-disable-next-line
-  const titel = `     **                     ** ** ********  **         \r\n    ****                   \/\/ \/\/ \/**\/\/\/\/\/  \/**  **   **\r\n   **\/\/**    ******  *****  ** **\/**       \/** \/\/** ** \r\n  **  \/\/**  **\/\/\/\/  **\/\/\/**\/**\/**\/*******  \/**  \/\/***  \r\n **********\/\/***** \/**  \/\/ \/**\/**\/**\/\/\/\/   \/**   \/**   \r\n\/**\/\/\/\/\/\/** \/\/\/\/\/**\/**   **\/**\/**\/**       \/**   **    \r\n\/**     \/** ****** \/\/***** \/**\/**\/**       ***  **     \r\n\/\/      \/\/ \/\/\/\/\/\/   \/\/\/\/\/  \/\/ \/\/ \/\/       \/\/\/  \/\/      `
+  //const title = `     **                     ** ** ********  **         \r\n    ****                   \/\/ \/\/ \/**\/\/\/\/\/  \/**  **   **\r\n   **\/\/**    ******  *****  ** **\/**       \/** \/\/** ** \r\n  **  \/\/**  **\/\/\/\/  **\/\/\/**\/**\/**\/*******  \/**  \/\/***  \r\n **********\/\/***** \/**  \/\/ \/**\/**\/**\/\/\/\/   \/**   \/**   \r\n\/**\/\/\/\/\/\/** \/\/\/\/\/**\/**   **\/**\/**\/**       \/**   **    \r\n\/**     \/** ****** \/\/***** \/**\/**\/**       ***  **     \r\n\/\/      \/\/ \/\/\/\/\/\/   \/\/\/\/\/  \/\/ \/\/ \/\/       \/\/\/  \/\/      `
+  //const title = `                                                                                   \n                                                                                   \n   ,---,                                                 ,---,.  ,--,              \n  \'  .\' \\                             ,--,    ,--,     ,\'  .\' |,--.\'|              \n /  ;    \'.                         ,--.\'|  ,--.\'|   ,---.\'   ||  | :              \n:  :       \\    .--.--.             |  |,   |  |,    |   |   .\':  : \'              \n:  |   /\\   \\  /  /    \'     ,---.  \`--\'_   \`--\'_    :   :  :  |  \' |        .--,  \n|  :  \' ;.   :|  :  /\`./    /     \\ ,\' ,\'|  ,\' ,\'|   :   |  |-,\'  | |      /_ ./|  \n|  |  ;/  \\   \\  :  ;_     /    / \' \'  | |  \'  | |   |   :  ;/||  | :   , \' , \' :  \n\'  :  | \\  \\ ,\'\\  \\    \`. .    \' /  |  | :  |  | :   |   |   .\'\'  : |__/___/ \\: |  \n|  |  \'  \'--\'   \`----.   \\\'   ; :__ \'  : |__\'  : |__ \'   :  \'  |  | \'.\'|.  \\  \' |  \n|  :  :        /  /\`--\'  /\'   | \'.\'||  | \'.\'|  | \'.\'||   |  |  ;  :    ; \\  ;   :  \n|  | ,\'       \'--\'.     / |   :    :;  :    ;  :    ;|   :  \\  |  ,   /   \\  \\  ;  \n\`--\'\'           \`--\'---\'   \\   \\  / |  ,   /|  ,   / |   | ,\'   ---\`-\'     :  \\  \\ \n                            \`----\'   ---\`-\'  ---\`-\'  \`----\'                 \\  \' ; \n                                                                             \`--\`  `
+  //const title = ` ______                          ____    ___                \n/\\  _  \\                  __  __/\\  _\`\\ /\\_ \\               \n\\ \\ \\_\\ \\    ____    ___ /\\_\\/\\_\\ \\ \\_\\_\\//\\ \\    __  __    \n \\ \\  __ \\  /\',__\\  /\'___\\/\\ \\/\\ \\ \\  _\\/ \\ \\ \\  /\\ \\/\\ \\   \n  \\ \\ \\/\\ \\/\\__, \`\\/\\ \\__/\\ \\ \\ \\ \\ \\ \\/   \\_\\ \\_\\ \\ \\_\\ \\  \n   \\ \\_\\ \\_\\/\\____/\\ \\____\\\\ \\_\\ \\_\\ \\_\\   /\\____\\\\/\`____ \\ \n    \\/_/\\/_/\\/___/  \\/____/ \\/_/\\/_/\\/_/   \\/____/ \`/___/> \\\n                                                      /\\___/\n                                                      \\/__/ `
+  const title = `_____/\\\\\\\\\\\\\\\\\\____________________________________________/\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\__/\\\\\\\\\\\\__________________        \n ___/\\\\\\\\\\\\\\\\\\\\\\\\\\_________________________________________\\/\\\\\\///////////__\\////\\\\\\__________________       \n  __/\\\\\\/////////\\\\\\_____________________________/\\\\\\__/\\\\\\_\\/\\\\\\________________\\/\\\\\\_______/\\\\\\__/\\\\\\_      \n   _\\/\\\\\\_______\\/\\\\\\__/\\\\\\\\\\\\\\\\\\\\_____/\\\\\\\\\\\\\\\\_\\///__\\///__\\/\\\\\\\\\\\\\\\\\\\\\\________\\/\\\\\\______\\//\\\\\\/\\\\\\__     \n    _\\/\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_\\/\\\\\\//////____/\\\\\\//////___/\\\\\\__/\\\\\\_\\/\\\\\\///////_________\\/\\\\\\_______\\//\\\\\\\\\\___    \n     _\\/\\\\\\/////////\\\\\\_\\/\\\\\\\\\\\\\\\\\\\\__/\\\\\\_________\\/\\\\\\_\\/\\\\\\_\\/\\\\\\________________\\/\\\\\\________\\//\\\\\\____   \n      _\\/\\\\\\_______\\/\\\\\\_\\////////\\\\\\_\\//\\\\\\________\\/\\\\\\_\\/\\\\\\_\\/\\\\\\________________\\/\\\\\\_____/\\\\_/\\\\\\_____  \n       _\\/\\\\\\_______\\/\\\\\\__/\\\\\\\\\\\\\\\\\\\\__\\///\\\\\\\\\\\\\\\\_\\/\\\\\\_\\/\\\\\\_\\/\\\\\\______________/\\\\\\\\\\\\\\\\\\_\\//\\\\\\\\/______ \n        _\\///________\\///__\\//////////_____\\////////__\\///__\\///__\\///______________\\/////////___\\////________`
   return (
     <div className="app">
       <header className="app-header">
         <pre>
-          {titel}
+          {title}
         </pre>
       </header>
 
-      {playerContent === "" ? <div className="input-container">
-        <div className="yt-url-input-container">
-          <form className="yt-url-input-form" onSubmit={(e) => { onYTUrlSubmit(e) }}>
-            <label className="yt-url-input-label" htmlFor="yt-url-input-input">Paste Youtube URL here</label>
-            <input id="yt-url-input-input" className="yt-url-input-input" type="text" onChange={(e) => onYTUrlChange(e.target.value)}></input>
-          </form>
+      {playerState === PlayerState.Empty ?
+        <div className="input-container">
+          <div className="yt-url-input-container">
+            <form className="yt-url-input-form" onSubmit={(e) => { onYTUrlSubmit(e) }}>
+              <label className="yt-url-input-label" htmlFor="yt-url-input-input">Paste Youtube URL here</label>
+              <input id="yt-url-input-input" className="yt-url-input-input" type="text" onChange={(e) => onYTUrlChange(e.target.value)}></input>
+            </form>
 
-          <div className="input-divider">
-            OR
-          </div>
+            <div className="input-divider">
+              OR
+            </div>
 
-          <div className="img-input-container">
+            <div className="img-input-container">
 
-            <div ref={imgInput}
-              className={"img-input-box " +
-                (fileIsHovering ?
-                  wrongFileType ? "img-input-with-wrong-file" : "img-input-with-file"
-                  : "img-input-without-file")}
-              style={{ cursor: 'pointer' }}
-              onClick={() => document.getElementById("file-input")?.click()}>
-              <input type="file"
-                accept="image/png, image/jpeg"
-                id="file-input"
-                style={{ "display": "none" }}
-                onChange={handleSelectImage}>
-              </input>
-              <div className="img-input-text">
-                {fileIsHovering ?
-                  wrongFileType ? "Unsupported file type" : "Drop the image now"
-                  : "Click to select or drag an image on this box"}
+              <div ref={imgInput}
+                className={"img-input-box " +
+                  (fileIsHovering ?
+                    wrongFileType ? "img-input-with-wrong-file" : "img-input-with-file"
+                    : "img-input-without-file")}
+                style={{ cursor: 'pointer' }}
+                onClick={() => document.getElementById("file-input")?.click()}>
+                <input type="file"
+                  accept="image/png, image/jpeg"
+                  id="file-input"
+                  style={{ "display": "none" }}
+                  onChange={handleSelectImage}>
+                </input>
+                <div className="img-input-text">
+                  {fileIsHovering ?
+                    wrongFileType ? "Unsupported file type" : "Drop the image now"
+                    : "Click to select or drag an image on this box"}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div> : null}
+        </div> :
 
-      <div className="player-container">
-        <pre
-          style={{ lineHeight: lineHeight + "px", fontSize: fontSize + "px" }}
-          className="ascii-player"
-        >
-          {playerContent}
-        </pre>
-        {audioUrl !== "" ?
-          <div className="audio-player">
-            <video autoPlay>
-              <source src={audioUrl} type="audio/webm"></source>
-            </video>
-          </div> :
-          null}
-      </div>
+        <div className="player-container">
+          <div
+            className="ascii-player"
+            style={playerState === PlayerState.Loading ? { position: 'relative' } : {}}
+          >
+            <pre
+              className={(playerState === PlayerState.Loading ? 'loading-screen' : '')}
+              style={{ lineHeight: lineHeight + "px", fontSize: fontSize + "px" }}
+            >
+              {playerContent}
+            </pre>
+          </div>
+          {audioUrl !== "" ?
+            <div className="audio-player">
+              <video ref={audioPlayer}>
+                <source src={audioUrl} type="audio/webm"></source>
+              </video>
+            </div> :
+            null}
+        </div>
+      }
     </div>
   );
 }
