8b6ce8eb576b57db1c36f21fc742e2d0e3c94081
André Esser <bepandre@hotmail.com>
2022-01-30 20:30:06 +0000

    stupid fix

--- server/app/ascii_utils.py
+++ server/app/ascii_utils.py
@@ -76,15 +76,14 @@ def find_best_video_quality_url(video_info: dict) -> dict:
             fps = format["fps"]
             found_formats["%s_%s_%s" % (codec, resolution, fps)] = format
             
-            # if resolution == preferred_resolutions[0]:
-            #     return format
-    return found_formats
+            if resolution == preferred_resolutions[0]:
+                return format
+
     for resolution in preferred_resolutions[1:]:
         for format in found_formats.values():
             if resolution == format["height"]:
                 return format
             
-    pprint.pprint("found_formats")
     raise Exception("format not found")
 
 def find_best_audio_quality_url(video_info: dict) -> dict:
