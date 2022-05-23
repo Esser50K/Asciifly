c6f4669a2aa55c96356e58818ba0e7647880ad96
mad <jetbrains@mad.solares.studio>
2022-05-23 20:27:22 +0200

    Added and set up new font

--- app/app/src/App.tsx
+++ app/app/src/App.tsx
@@ -6,8 +6,36 @@ import Layout from "./pages/Layout";
 import NoPage from "./pages/NoPage";
 import Watch from "./pages/Watch";
 import {createTheme, ThemeProvider} from '@material-ui/core/styles';
+import PixelatedTimesNewRomanTTF from './fonts/pixelated-times-new-roman.ttf'
 
-const theme = createTheme({});
+const ptnr = {
+  fontFamily: 'Pixelated Times New Roman',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  src: `
+    local('Raleway'),
+    local('Raleway-Regular'),
+    url(${PixelatedTimesNewRomanTTF}) format('woff2')
+  `,
+  unicodeRange:
+      'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+}
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Pixelated Times New Roman', 'Times New Roman'",
+    fontSize: 24,
+    allVariants: {
+      lineHeight: 0.74,
+    }
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': [ptnr],
+      },
+    },
+  },
+});
 
 function App() {
   return (
