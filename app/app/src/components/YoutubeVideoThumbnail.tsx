94d0b9afddcd9169526c32d32b4c11fdddad7dc6
mad <jetbrains@mad.solares.studio>
2022-05-23 11:36:47 +0200

    Added thumbnail pictures for vid grid Added hover state for YoutubeVideoThumbnail Replaced Link navigation with programmatic one in YoutubeVideoThumbnail component

--- app/app/src/components/YoutubeVideoThumbnail.tsx
+++ app/app/src/components/YoutubeVideoThumbnail.tsx
@@ -3,10 +3,17 @@ import Box from "@material-ui/core/Box";
 import Avatar from "@material-ui/core/Avatar";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        thumbnailContainer: {
+            cursor: 'pointer',
+            transition: 'transform 0.2s',
+            '&:hover': {
+                transform: 'scale(1.05)',
+            }
+        },
         thumbnail: {
             backgroundSize: "cover",
             '&::before': {
@@ -16,7 +23,7 @@ const useStyles = makeStyles((theme: Theme) =>
                 height: 0,
                 paddingBottom: "calc(100% / (16/9))",
             },
-            marginBottom: theme.spacing(1.5)
+            marginBottom: theme.spacing(1.5),
         }
     })
 )
@@ -35,24 +42,24 @@ interface YoutubeVideoThumbnailProps {
 
 function YoutubeVideoThumbnail({ youtubeId, title, thumbnailImage, avatarImage }: YoutubeVideoThumbnailProps) {
     const classes = useStyles();
+    const navigate = useNavigate();
+
+    const navigateToWatch = () => navigate(`/watch?v=${youtubeId}`)
 
     return (
-        <Link to={`/watch?v=${youtubeId}`}>
-            <Box bgcolor="background.paper">
-                <Box className={classes.thumbnail} style={{backgroundImage: `url('${thumbnailImage.src}')`}} />
-                <Grid container spacing={1}>
-                    <Grid item xs={2}>
-                        <Avatar alt={avatarImage.alt} src={avatarImage.src} />
-                    </Grid>
-                    <Grid item xs={10}>
-                        <Typography variant="subtitle2" color="textPrimary">
-                            { title }
-                        </Typography>
-                    </Grid>
+        <Box className={classes.thumbnailContainer} bgcolor="background.paper" onClick={navigateToWatch}>
+            <Box className={classes.thumbnail} style={{backgroundImage: `url('${thumbnailImage.src}')`}} />
+            <Grid container spacing={1}>
+                <Grid item xs={2}>
+                    <Avatar alt={avatarImage.alt} src={avatarImage.src} />
                 </Grid>
-            </Box>
-        </Link>
-
+                <Grid item xs={10}>
+                    <Typography variant="subtitle2" color="textPrimary">
+                        { title }
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
 
--- app/app/src/pages/Home.tsx
+++ app/app/src/pages/Home.tsx
@@ -24,7 +24,7 @@ function Home() {
                 youtubeId: 'ASJ3iY0-qpQ',
                 title: 'Turning videos into TEXT? 🔠',
                 thumbnailImage: {
-                    src: '/images/video-thumbnails/thumbnail.webp'
+                    src: '/images/video-thumbnails/1.webp'
                 },
                 avatarImage: {
                     alt: 'Esser50k',
@@ -35,10 +35,10 @@ function Home() {
         {
             id: '2',
             props: {
-                youtubeId: 'ASJ3iY0-qpQ',
-                title: 'Turning videos into TEXT? 🔠',
+                youtubeId: '5nnVj1i3Si4',
+                title: 'BANNED from Twitter for being offensive',
                 thumbnailImage: {
-                    src: '/images/video-thumbnails/thumbnail.webp'
+                    src: '/images/video-thumbnails/2.webp'
                 },
                 avatarImage: {
                     alt: 'Esser50k',
@@ -49,10 +49,10 @@ function Home() {
         {
             id: '3',
             props: {
-                youtubeId: 'ASJ3iY0-qpQ',
-                title: 'Turning videos into TEXT? 🔠',
+                youtubeId: 'hId3HtBwPKo',
+                title: 'Neighbour got Caught! 😱',
                 thumbnailImage: {
-                    src: '/images/video-thumbnails/thumbnail.webp'
+                    src: '/images/video-thumbnails/3.webp'
                 },
                 avatarImage: {
                     alt: 'Esser50k',
@@ -63,10 +63,10 @@ function Home() {
         {
             id: '4',
             props: {
-                youtubeId: 'ASJ3iY0-qpQ',
-                title: 'Turning videos into TEXT? 🔠',
+                youtubeId: 'CxMRcBD5sE8',
+                title: 'Detecting Faces in Thousands of Videos FAST #python',
                 thumbnailImage: {
-                    src: '/images/video-thumbnails/thumbnail.webp'
+                    src: '/images/video-thumbnails/4.webp'
                 },
                 avatarImage: {
                     alt: 'Esser50k',
@@ -77,10 +77,10 @@ function Home() {
         {
             id: '5',
             props: {
-                youtubeId: 'ASJ3iY0-qpQ',
-                title: 'Turning videos into TEXT? 🔠',
+                youtubeId: 'if2q0XxCWQ0',
+                title: '#Python Tips, Tricks and Quirks - Hackertalk',
                 thumbnailImage: {
-                    src: '/images/video-thumbnails/thumbnail.webp'
+                    src: '/images/video-thumbnails/5.webp'
                 },
                 avatarImage: {
                     alt: 'Esser50k',
@@ -92,9 +92,9 @@ function Home() {
             id: '6',
             props: {
                 youtubeId: 'ASJ3iY0-qpQ',
-                title: 'Turning videos into TEXT? 🔠',
+                title: 'Exploiting my Cat for Fun and Profit #shorts',
                 thumbnailImage: {
-                    src: '/images/video-thumbnails/thumbnail.webp'
+                    src: '/images/video-thumbnails/6.webp'
                 },
                 avatarImage: {
                     alt: 'Esser50k',
@@ -106,9 +106,9 @@ function Home() {
             id: '7',
             props: {
                 youtubeId: 'ASJ3iY0-qpQ',
-                title: 'Turning videos into TEXT? 🔠',
+                title: 'Smart Home Cat turns on Lights #shorts',
                 thumbnailImage: {
-                    src: '/images/video-thumbnails/thumbnail.webp'
+                    src: '/images/video-thumbnails/7.webp'
                 },
                 avatarImage: {
                     alt: 'Esser50k',
@@ -120,9 +120,9 @@ function Home() {
             id: '8',
             props: {
                 youtubeId: 'ASJ3iY0-qpQ',
-                title: 'Turning videos into TEXT? 🔠',
+                title: 'Saving Sushi from Doom #shorts',
                 thumbnailImage: {
-                    src: '/images/video-thumbnails/thumbnail.webp'
+                    src: '/images/video-thumbnails/8.webp'
                 },
                 avatarImage: {
                     alt: 'Esser50k',
